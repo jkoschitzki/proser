3fe11f71b871dd43138bd2c68ffcd1ac3970d874
Jana Koschitzki <janakoschitzki@gmx.de>
2026-05-18 15:03:20 +0200

    initial commit: setup maven project my-prose-app

---
+++ my-prose-app/src/main/java/de/htw_berlin/fb4/jkos/proser/App.java
@@ -0,0 +1,13 @@
+package de.htw_berlin.fb4.jkos.proser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
